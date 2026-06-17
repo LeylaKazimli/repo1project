e6d78e2d8d0ab6d07c5059d0acb950f1258a2f64
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-17 23:14:16 +0400

    xetalikod

---
+++ src/xetalikod.java
@@ -0,0 +1,15 @@
+public class xetalikod {
+    public static void main(String[] args) {
+
+        int yas = 25;                    // Xəta 1 int kicik herfle olmalidi
+        String sehir = "Baku";           // Xəta 2 string dirnaq icinde olmalidi
+        double kreditMeblegi = 5000;     // Xəta 6 bitisik olmalidi
+        boolean aktiv = true;            // Xəta 4 kicik herfle true
+
+        final int LIMIT = 100;
+
+        // LIMIT = 200;                  // Xəta 5 limit deyise bilmez
+
+        System.out.println(yas);         // Xəta 3 noqte vergul olmalidi sonda
+    }
+}
