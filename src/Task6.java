7f161ac4b9a44c1e840df272496c357015646fe7
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-17 23:45:19 +0400

    Task6

---
+++ src/Task6.java
@@ -0,0 +1,15 @@
+public class Task6 {
+    public static void main(String[] args) {
+
+        int a = 17;
+        int b = 5;
+
+        System.out.println("Toplama: " + (a + b));
+        System.out.println("Cixma: " + (a - b));
+        System.out.println("Vurma: " + (a * b));
+        System.out.println("Bolme: " + (a / b));
+        System.out.println("Qaliq: " + (a % b));
+
+        // int / int = int oldugu ucun 17/5 = 3 cixir
+    }
+}
