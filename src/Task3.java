87f6d3d25af291e013ee86631b270c4dd626a1b8
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-17 22:56:40 +0400

    add Task3

---
+++ src/Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+        final int MIN_KREDIT = 300;
+        final int MAX_KREDIT = 50000;
+        final double ILLIK_STANDART_FAIZ = 10.9;
+        final String BANK_QISA_ADI = "ABB";
+        final int MAX_KREDIT_MUDDETI = 60;
+
+        System.out.println("Min kredit: " + MIN_KREDIT);
+        System.out.println("Max kredit: " + MAX_KREDIT);
+        System.out.println("Illik faiz: " + ILLIK_STANDART_FAIZ);
+        System.out.println("Bank adi: " + BANK_QISA_ADI);
+        System.out.println("Max muddet: " + MAX_KREDIT_MUDDETI);
+
+    }
+}
