0b6d4e30dfe12cc7678d2fa3d752ba1505ab4207
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-17 21:49:10 +0400

    Add Task1 bank account information

---
+++ src/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+    public static void main(String[] args) {
+        String musteri = "Leyla Kazimli";
+        int hesabnomresi = 123456;
+        double balans = 3842.50;
+        boolean aktivdir = true;
+        String hesabnovu = "Emanet";
+
+        System.out.println("Musteri: " + musteri);
+        System.out.println("Hesab nomresi: " + hesabnomresi);
+        System.out.println("Balans: " + balans);
+        System.out.println("Aktivdir: " + aktivdir);
+        System.out.println("Hesab novu: " + hesabnovu);
+    }
+}
