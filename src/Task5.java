b55400ac4d729412faf6226ca55054ef2beccf36
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-17 23:24:40 +0400

    Task5

---
+++ src/Task5.java
@@ -0,0 +1,19 @@
+public class Task5 {
+    public static void main(String[] args) {
+
+        char bashHerf = 'L';
+        String tamAd = "Leyla Kazimli";
+        int yas = 31;
+        double ayliqGelir = 7500.50;
+        boolean vipMusteri = true;
+        int musteriKodu = 12345;
+
+        System.out.println("Bas herf: " + bashHerf);
+        System.out.println("Tam ad: " + tamAd);
+        System.out.println("Yas: " + yas);
+        System.out.println("Ayliq gelir: " + ayliqGelir);
+        System.out.println("VIP Musteri: " + vipMusteri);
+        System.out.println("Musteri kodu: " + musteriKodu);
+
+    }
+}
