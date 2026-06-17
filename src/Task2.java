c86d17486b3cbc0058c6d536ed2f5a4496591c94
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-17 22:49:43 +0400

    Add Task2

--- src/Task2.java
+++ src/Task2.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Task2 {
+    public static void main(String[] args) {
+
+        int bal = 1000;
+
+        System.out.println("Baslangic: " + bal);
+
+        bal += 500;
+        System.out.println("500 elave: " + bal);
+
+        bal -= 200;
+        System.out.println("200 cixdi: " + bal);
+
+        bal *= 2;
+        System.out.println("2 vuruldu: " + bal);
+
+        bal /= 4;
+        System.out.println("4-e bolundu: " + bal);
+
+    }
 }
