8c37bb3824bd069967e0267e789e7aff1d7307f7
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-18 22:10:23 +0400

    Task10

---
+++ src/Task10.java
@@ -0,0 +1,35 @@
+public class Task10 {
+    public static void main(String[] args) {
+
+        int umumiDeqiqe = 145;
+
+        int saat = umumiDeqiqe / 60;
+        int deqiqe = umumiDeqiqe % 60;
+
+        System.out.println("145 dəqiqə");
+        System.out.println("Saat: " + saat);
+        System.out.println("Dəqiqə: " + deqiqe);
+
+        System.out.println();
+
+        umumiDeqiqe = 90;
+
+        saat = umumiDeqiqe / 60;
+        deqiqe = umumiDeqiqe % 60;
+
+        System.out.println("90 dəqiqə");
+        System.out.println("Saat: " + saat);
+        System.out.println("Dəqiqə: " + deqiqe);
+
+        System.out.println();
+
+        umumiDeqiqe = 200;
+
+        saat = umumiDeqiqe / 60;
+        deqiqe = umumiDeqiqe % 60;
+
+        System.out.println("200 dəqiqə");
+        System.out.println("Saat: " + saat);
+        System.out.println("Dəqiqə: " + deqiqe);
+    }
+}
