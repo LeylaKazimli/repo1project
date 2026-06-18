8f9a5d13c1a5089f86504364a14a1b41d28976a2
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-18 23:37:28 +0400

    Task15

---
+++ src/Task15.java
@@ -0,0 +1,24 @@
+public class Task15 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        boolean x = true;
+        boolean y = false;
+
+        String s = "Java";
+
+        System.out.println(a + b * 2);          // 16
+        System.out.println(a / b);              // 3
+        System.out.println(a % b);              // 1
+        System.out.println(x && !y);            // true
+        System.out.println(!x || y);            // false
+        System.out.println(a > 5 && b < 5);     // true
+        System.out.println(s + a);              // Java10
+        System.out.println("3" + 4);            // 34
+        System.out.println(3 + 4 + "!");        // 7!
+        System.out.println("!" + 3 + 4);        // !34
+
+    }
+}
