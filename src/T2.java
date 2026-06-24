9e24f661179316e1082593c47d35baf421cc2bdb
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-24 23:16:07 +0400

    T2

---
+++ src/T2.java
@@ -0,0 +1,11 @@
+public class T2 {
+    public static void main(String[] args) {
+        int x = 36, y = 6;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
