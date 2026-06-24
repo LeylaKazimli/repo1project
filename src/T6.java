07122be40a45940429579d46477fe32a196b8122
LeylaKazimli <kazimilleyla@gmail.com>
2026-06-24 23:36:21 +0400

    T6

---
+++ src/T6.java
@@ -0,0 +1,21 @@
+public class T6 {
+    public static void main(String[] args) {
+
+        final double PI = 3.14159;
+        final int MAX_DENEME = 3;
+        final String BANK_KODU = "ABB01";
+
+        System.out.println("PI = " + PI);
+        System.out.println("MAX_DENEME = " + MAX_DENEME);
+        System.out.println("BANK_KODU = " + BANK_KODU);
+
+        // PI = 3.14;
+        // Xəta: Cannot assign a value to final variable 'PI'
+
+        // MAX_DENEME = 5;
+        // Xəta: Cannot assign a value to final variable 'MAX_DENEME'
+
+        // BANK_KODU = "ABB02";
+        // Xəta: Cannot assign a value to final variable 'BANK_KODU'
+    }
+}
